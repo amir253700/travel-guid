e9da159235efbd9817daa2178f6e0e5df2eef55f
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-09 00:39:11 +0100

    Create the main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Grid } from "@mui/material";
+
+import Map from "./components/Map/Map";
+import Header from "./components/Header/Header";
+import PlaceList from "./components/PlaceList/PlaceList";
+import "./app.css";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Header />
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <PlaceList />
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <Map />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default App;
